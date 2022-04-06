0aef853079c647610d57c69183e7fad09630bb96
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-06 15:33:15 +0200

    Initial commit

---
+++ src/main/scala/Homework.scala
@@ -0,0 +1,25 @@
+object Homework extends App {
+  import org.apache.spark.sql.SparkSession
+  import org.apache.spark.sql.functions._
+
+  val path = if (args.length > 0) (args(0))
+  else ("Sample100.csv")
+
+  val spark = SparkSession
+    .builder()
+    .appName("Spark SQL basic example")
+    .master("local[*]")
+    .getOrCreate()
+
+  val filePrepared = spark
+    .read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv(path)
+
+  val dfSorted = filePrepared
+    .sort(desc("leave")).show()
+
+  val dfGrouped = filePrepared
+    .groupBy("Company Name").max().show()
+}
